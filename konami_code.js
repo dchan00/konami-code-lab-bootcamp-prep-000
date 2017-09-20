df9cae8a21c3cc9a49f5d2fff2d41ee085bdf099
DANIEL CHAN <dchan513@msn.com>
2017-09-20 02:03:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-//const test = document.getElementByTagName('body')
+const test = document.getElementByTagName('body')
 
 function init() {
   // Write your JavaScript code inside the init() function
