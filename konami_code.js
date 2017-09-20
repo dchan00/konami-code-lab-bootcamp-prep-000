077c6a9435f08d49938c0e751068a457e72fd6df
DANIEL CHAN <dchan513@msn.com>
2017-09-20 02:01:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+const test = document.getElementByTagName('body')
+
 function init() {
   // Write your JavaScript code inside the init() function
 
   let index =0;
-  const test = document.getElementByTagName('body')
   test.addEventListener('keydown', onKeyDownHandler)
 
 }
