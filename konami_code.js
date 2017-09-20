6ed7964629bf97eb326bc4535ee06715395d4c59
DANIEL CHAN <dchan513@msn.com>
2017-09-20 02:32:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-//&&((%'%'BA
+//up, up, down down, left, right left right B A
 
 
 function init() {
@@ -9,7 +9,7 @@ function init() {
   const test = document.querySelector('body')
   test.addEventListener('keydown', function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
-
+    console.log(key)
 
     if(key === alphabet[index]){
       index++;
