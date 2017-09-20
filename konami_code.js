a2d4936229a3a10605029886899fbbf91e358a7e
DANIEL CHAN <dchan513@msn.com>
2017-09-20 02:27:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -9,9 +9,7 @@ function init() {
   const test = document.querySelector('body')
   test.addEventListener('keydown', function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
-    if(key === 16) {
-      return key.preventDefault()
-    }
+    
 
     if(key === alphabet[index]){
       index++;
